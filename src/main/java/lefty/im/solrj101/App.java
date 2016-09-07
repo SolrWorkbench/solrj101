27d3c28cb708ab124ce55ba248809a0804fec5ef
leftygbalogh <lefty@lefty-Inspiron-5558>
2016-09-07 05:21:41 +0100

    still experimental github connection and maven settings

---
+++ src/main/java/lefty/im/solrj101/App.java
@@ -0,0 +1,13 @@
+package lefty.im.solrj101;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
